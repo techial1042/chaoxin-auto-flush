fb553a95ae19d452e3cf154fff68baaa646f6c1c
techial <dev@techial.cc>
2020-12-03 20:16:21 +0800

    polish

--- ChaoXing.py
+++ ChaoXing.py
@@ -25,6 +25,9 @@ class ChaoXing:
         self.dump_session()
 
     def __init_session(self):
+        """
+        session 初始化，session 共享
+        """
         headers = {
             'Connection': 'keep-alive',
             'DNT': '1',
@@ -38,6 +41,9 @@ class ChaoXing:
         self.session.headers = headers
 
     def __load_session(self, username, password):
+        """
+        从本地磁盘加载 session，若文件不在，则 HTTP 登录
+        """
         if os.path.exists("cookie.bin"):
             with open('cookie.bin', 'rb') as f:
                 self.session.cookies.update(pickle.load(f))
@@ -47,10 +53,16 @@ class ChaoXing:
             print("Session from login request.")
 
     def dump_session(self):
+        """
+        将 session 保存到本地磁盘
+        """
         with open('cookie.bin', 'wb') as f:
             pickle.dump(self.session.cookies, f)
 
     def __login(self, username, password):
+        """
+        超星登录接口
+        """
         payload = {
             'fid': '2182',
             'uname': username,
@@ -63,6 +75,9 @@ class ChaoXing:
         print('Http code: {}, text: {}'.format(login.status_code, login.text))
 
     def __get_my_arg(self):
+        """
+        获得课程信息
+        """
         url = 'https://mooc1-1.chaoxing.com/knowledge/cards'
         requests_params = {
             'clazzid': self.clazz_id,
@@ -73,6 +88,9 @@ class ChaoXing:
         return self.__arg_handler(result)
 
     def __arg_handler(self, text):
+        """
+        从 HTML 中获得参数
+        """
         arg = re.findall('mArg = (.*);', text, re.MULTILINE)[1]
         print('args = \n{}'.format(arg))
         self.arg_json = json.loads(arg)
@@ -85,8 +103,8 @@ class ChaoXing:
             object_id = attachment['property']['objectid']
 
             status_json = self.__get_play_status(object_id)
-            params = self.get_params(attachment['jobid'], attachment['property']['objectid'], 0,
-                                     status_json['duration'], None, None, attachment['otherInfo'], None, 0)
+            params = self.__get_params(attachment['jobid'], attachment['property']['objectid'], 0,
+                                       status_json['duration'], None, None, attachment['otherInfo'], None, 0)
             audio_play = self.session.get(
                 url='https://mooc1-1.chaoxing.com/multimedia/log/a/{}/{}'.format(cpi, status_json['dtoken']),
                 params=params)
@@ -94,8 +112,8 @@ class ChaoXing:
             print('boom, object_id: {}, params: {}'.format(object_id, params))
             print('boom, result {}'.format(audio_play.text))
             status_json = self.__get_play_status(object_id)
-            params = self.get_params(attachment['jobid'], attachment['property']['objectid'], status_json['duration'],
-                                     status_json['duration'], None, None, attachment['otherInfo'], None, 4)
+            params = self.__get_params(attachment['jobid'], attachment['property']['objectid'], status_json['duration'],
+                                       status_json['duration'], None, None, attachment['otherInfo'], None, 4)
             audio_play = self.session.get(
                 url='https://mooc1-1.chaoxing.com/multimedia/log/a/{}/{}'.format(cpi, status_json['dtoken']),
                 params=params)
@@ -106,13 +124,19 @@ class ChaoXing:
             time.sleep(random.randint(3, 10))
 
     def __get_play_status(self, object_id):
+        """
+        获得视频 / 音频基本信息
+        """
         status = self.session.get('https://mooc1-1.chaoxing.com/ananas/status/{}'.format(object_id),
                                   params={'_dc': int(time.time() * 1000)})
         print('student study url = {}'.format(status.url))
         return status.json()
 
-    def get_params(self, job_id: str, object_id: str, playing_time: int, duration: int, start_time, end_time,
-                   other_info, rt, is_drag):
+    def __get_params(self, job_id: str, object_id: str, playing_time: int, duration: int, start_time, end_time,
+                     other_info, rt, is_drag):
+        """
+        请求参数构建
+        """
         clip_time = ChaoXing.__get_clip_time(duration, start_time, end_time)
         view = 'pc'
         enc = self.__get_enc(job_id, object_id, playing_time, duration, start_time, end_time)
@@ -137,6 +161,9 @@ class ChaoXing:
         }
 
     def __get_enc(self, job_id: str, object_id: str, playing_time: int, duration: int, start_time, end_time):
+        """
+        enc 参数加密
+        """
         if job_id is None:
             job_id = ""
 
@@ -155,6 +182,9 @@ class ChaoXing:
 
     @staticmethod
     def __get_clip_time(duration: int, start_time, end_time):
+        """
+        clip_time 参数构建
+        """
         if start_time is None:
             start_time = "0"
 
